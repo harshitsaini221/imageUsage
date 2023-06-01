00f2ecb7fb9ce7e7f8e7ed4589748674a6f8ee35
Harshit Saini <harshitsaini221@gmail.com>
2023-06-01 20:27:43 +0530

    Update location.js

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAB83Du6YLtYs6gvMaRJKvcxcnpYKKytVM";
+const GOOGLE_API_KEY = "Use Your Own API Key";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
